99a685642a8bd993caa1ab2ac546715e674fb03a
Alexandre Bevington <alexandre.bevington@gov.bc.ca>
2024-02-13 17:31:34 -0800

    no buf

--- app_2_db.R
+++ app_2_db.R
@@ -31,7 +31,7 @@ conn <- pool::dbPool(
 
 # POL ####
 
-  postgis_get_pol <- function(to_clip = "fwa_wetlands",
+  postgis_get_pol <- function(to_clip = "fire",
                               to_clip_cols_to_keep = "*",
                               elev = "T",
                               my_wkt = new_ws2_wkt,
@@ -39,19 +39,22 @@ conn <- pool::dbPool(
                               my_dem = dem){
 
   q <- paste0("SELECT w.*, ST_Intersection(w.geom,ST_GeomFromText('", my_wkt, "', 3005)) AS geom
-               FROM ", to_clip, " w WHERE ST_Intersects(w.geom,ST_GeomFromText('", my_wkt, "', 3005))")
+                 FROM ", to_clip, " w WHERE ST_Intersects(w.geom,ST_GeomFromText('", my_wkt, "', 3005))")
+
   o <-  st_read(conn, query = q, quiet = T)
 
   if(nrow(o) > 0) {
     o <- o %>% filter(st_geometry_type(.) %in% c("POLYGON", "MULTIPOLYGON")) %>%
       ms_explode() %>% mutate(clipped_area_m2 = as.numeric(st_area(.))) %>%
       filter(clipped_area_m2 > min_area_km2 * (1000 * 1000))
+    # print(o %>% head())
   if(nrow(o) > 0) {
     if(elev == "T") {
       o <- o %>% bind_cols(terra::extract(my_dem, o %>% st_centroid(of_largest_polygon = T) %>% st_transform(4326)) %>%
                              dplyr::select(COP_GLO30_FOR_watershedBC_cog) %>%
                              rename(elevation = COP_GLO30_FOR_watershedBC_cog))}
-      o <- o  %>% st_buffer(1) %>% st_cast("POLYGON")}}
+      o <- o  %>% #st_buffer(1) %>%
+        st_cast("POLYGON")}}
   if(nrow(o) > 0) {
     o <- o %>% filter(clipped_area_m2 > 10) %>%
       mutate(type = to_clip) %>%
@@ -59,7 +62,7 @@ conn <- pool::dbPool(
     return(o)
     }else{
       o <- st_as_sf(data.frame(lon = -111, lat = 55), coords = c("lon", "lat"), crs = 4326) %>% mutate(type = "test") %>% filter(type != "test")
-      }
+    }
   return(o)
   }
 
