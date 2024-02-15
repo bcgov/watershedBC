e69a29d9f20e01eb5aa03f88ab5530ae93f587a0
Alexandre Bevington <alexandre.bevington@gov.bc.ca>
2024-02-14 17:56:07 -0800

    other LS

--- app_5_leaflet.R
+++ app_5_leaflet.R
@@ -15,32 +15,37 @@ initial_map <- leaflet() %>%
              primaryAreaUnit = "hectares",
              secondaryAreaUnit = "sqmeters",
              position = "topleft") %>%
-  leafem:::addCOG(url = "https://bcbasin.s3.ca-central-1.amazonaws.com/COP_GLO30_FOR_watershedBC_cog.tif",
-                group = "GLO-30 DEM",
-                resolution = res,
-                opacity = 1,
-                autozoom = F) %>%
-  leafem:::addCOG(url = "https://bcbasin.s3.ca-central-1.amazonaws.com/BC_2023v2_4326_v2_bigTiff_JPEG.tif",
-                  group = "Sentinel 2023 (slow)",
-                  resolution = res,
-                  opacity = 1,
-                  autozoom = F) %>%
-  leafem:::addCOG(url = "https://bcbasin.s3.ca-central-1.amazonaws.com/1985_1990v3_COG_AV_JP_BIG.tif",
-                  group = "Landsat 1985-1990 (slow)",
-                  resolution = res,
-                  opacity = 1,
-                  autozoom = F) %>%
-  leafem:::addCOG(url = "https://bcbasin.s3.ca-central-1.amazonaws.com/2020_2023v3_COG_AV_JP_BIG.tif",
-                  group = "Landsat 2020-2023 (slow)",
-                  resolution = res,
-                  opacity = 1,
-                  autozoom = F) %>%
+  addWMSTiles("https://storage.googleapis.com/earthenginepartners-hansen/tiles/gfc_v1.4/first_543/{z}/{x}/{y}.jpg",
+              layers = "GRB_BSK", options = WMSTileOptions(format = "image/png", transparent = TRUE), group = "Landsat 2000") %>%
+    addWMSTiles("https://storage.googleapis.com/earthenginepartners-hansen/tiles/gfc_v1.9/last_543/{z}/{x}/{y}.jpg",
+                layers = "GRB_BSK", options = WMSTileOptions(format = "image/png", transparent = TRUE), group = "Landsat 2021") %>%
+  # leafem:::addCOG(url = "https://bcbasin.s3.ca-central-1.amazonaws.com/COP_GLO30_FOR_watershedBC_cog.tif",
+  #               group = "GLO-30 DEM",
+  #               resolution = res,
+  #               opacity = 1,
+  #               autozoom = F) %>%
+  # leafem:::addCOG(url = "https://bcbasin.s3.ca-central-1.amazonaws.com/BC_2023v2_4326_v2_bigTiff_JPEG.tif",
+  #                 group = "Sentinel 2023 (slow)",
+  #                 resolution = res,
+  #                 opacity = 1,
+  #                 autozoom = F) %>%
+  # leafem:::addCOG(url = "https://bcbasin.s3.ca-central-1.amazonaws.com/1985_1990v3_COG_AV_JP_BIG.tif",
+  #                 group = "Landsat 1985-1990 (slow)",
+  #                 resolution = res,
+  #                 opacity = 1,
+  #                 autozoom = F) %>%
+  # leafem:::addCOG(url = "https://bcbasin.s3.ca-central-1.amazonaws.com/2020_2023v3_COG_AV_JP_BIG.tif",
+  #                 group = "Landsat 2020-2023 (slow)",
+  #                 resolution = res,
+  #                 opacity = 1,
+  #                 autozoom = F) %>%
   addCircleMarkers(data = wsc_pp_ac, lng = wsc_pp_ac$lon, lat = wsc_pp_ac$lat, color = "steelblue", radius = 3, group = "WSC Active",
                    label = paste0(wsc_pp_ac$name, " - ", wsc_pp_ac$stationnum, " [active]")) %>%
   addCircleMarkers(data = wsc_pp_dc, lng = wsc_pp_dc$lon, lat = wsc_pp_dc$lat, color = "grey", radius = 3, group = "WSC Discontinued",
                    label = paste0(wsc_pp_dc$name, " - ", wsc_pp_dc$stationnum, " [discontinued]")) %>%
-  addLayersControl(baseGroups = c("BC Basemap", "WorldImagery", "WorldTopoMap"),
-                   overlayGroups = c("GLO-30 DEM","Sentinel 2023 (slow)", "Landsat 2020-2023 (slow)", "Landsat 1985-1990 (slow)", "WSC Active", "WSC Discontinued"),
+  addLayersControl(baseGroups = c("BC Basemap", "WorldImagery", "WorldTopoMap", "Landsat 2000", "Landsat 2021"),
+                   # overlayGroups = c("GLO-30 DEM","Sentinel 2023 (slow)", "Landsat 2020-2023 (slow)", "Landsat 1985-1990 (slow)",
+                   overlayGroups = c("WSC Active", "WSC Discontinued"),
                    options = layersControlOptions(collapsed = F)) %>%
   hideGroup(c("GLO-30 DEM","Sentinel 2023 (slow)","Landsat 1985-1990 (slow)","Landsat 2020-2023 (slow)","WSC Active","WSC Discontinued")) %>%
   addMouseCoordinates()
