94eb44ee194bb47e2f8c3208b78113cbf23c3ebf
Scott Howes <scott@voxelops.com>
2024-03-26 10:30:07 -0700

    docker init for db

---
+++ database/db_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Running SQL scripts to create app db
+echo ""
+echo "PUBLIC SCHEMA SETUP..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file /tmp/sql/public_setup.sql
+echo "DONE"
+
+echo ""
+echo "PUBLIC TABLE CREATION..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file /tmp/sql/public_tables.sql
+echo "DONE"
+
+# echo ""
+# echo "PUBLIC USER CREATION..."
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file /tmp/sql/public_users.sql
+# echo "DONE"
+echo ""
